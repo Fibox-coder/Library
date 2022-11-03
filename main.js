4e381cd357499ba4a2ef9f90104d4706f22bb5ab
Tim D <timvandeutekom@hotmail.com>
2022-11-03 09:39:23 +0100

    initial commit with html and css base

---
+++ main.js
@@ -0,0 +1,30 @@
+let myLibrary = []
+
+function Book(title, author, pages, haveRead){
+
+}
+
+
+
+function addBookToLibrary(){
+
+}
+
+// function User(email, name){
+//   this.email = email;
+//   this.name = name;
+//   this.online = false;
+// }
+
+// function Book(title, author, pages, haveRead){
+//   this.title = title
+//   this.author = author
+//   this.pages = pages
+//   this.haveRead = haveRead
+//   this.info = function(){
+//     return `${title} by  ${author}, ${pages}, ${haveRead}`
+//   }
+// }
+
+// const Book1 = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet')
+// console.log(Book1.info())
