da6cb548e861a5ee58eacf52ed241a525e7903b7
Tim D <timvandeutekom@hotmail.com>
2022-11-18 11:00:14 +0100

    Adds local storage, read button toggle and correct remove splice

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const overlay = document.getElementById("addOverlay")
 const submit = document.getElementById("submitBtn")
 const gridBooks = document.getElementById("grid-books")
 let removeBtn = document.createElement('button')
-
 let myLibrary = []
 
 function Book(title, author, pages, haveRead){
@@ -13,10 +12,9 @@ function Book(title, author, pages, haveRead){
   this.pages = pages
   this.haveRead = haveRead
 }
+
 let bookOne = new Book('The Hobbit', 'J.R.R. Tolkien', '295', false)
 let bookTwo = new Book('Game of Thrones', 'George R. R. Martin', '694 pages', true)
-
-
 myLibrary.push(bookOne)
 myLibrary.push(bookTwo)
 
@@ -24,64 +22,77 @@ function addBookToLibrary(){
   const nextBook = getInput()
   myLibrary.push(nextBook)
   console.log(myLibrary) 
-  createBookItem(nextBook)
+  createBookItem(nextBook, myLibrary.lastIndexOf(nextBook))
+  setData(); //saves updated array in local storage
   return myLibrary
   }
 
-  // Creates the layout for one Book
-  function createBookItem(book, index){
-    let bookCard = document.createElement('div')
-    let title = document.createElement('h4')
-    let author = document.createElement('p')
-    let pages = document.createElement('p')
-    let readBtn = document.createElement('button')
-    let removeBtn = document.createElement('button')
-   
+// Creates the layout for one Book
+function createBookItem(book, index){
+  let bookCard = document.createElement('div')
+  let title = document.createElement('h4')
+  let author = document.createElement('p')
+  let pages = document.createElement('p')
+  let readBtn = document.createElement('button')
+  let removeBtn = document.createElement('button')
+  
 
-    bookCard.setAttribute('id', index)
+  bookCard.setAttribute('id', index)
 
-    bookCard.classList.add('book-card')
-    readBtn.classList.add('read')
-    removeBtn.classList.add('remove') 
+  bookCard.classList.add('book-card')
+  readBtn.classList.add('read')
+  removeBtn.classList.add('remove') 
 
-    title.textContent = `${book.title}`
-    author.textContent = `By: ${book.author}`
-    pages.textContent = `number of pages: ${book.pages}`
-    removeBtn.textContent = "Remove"
+  title.textContent = `${book.title}`
+  author.textContent = `By: ${book.author}`
+  pages.textContent = `number of pages: ${book.pages}`
+  removeBtn.textContent = "Remove"
 
 
-      if (book.haveRead === true){
-        readBtn.textContent = "Read"
-        readBtn.style.cssText = "background-color: lightgreen;"
-      } else {
-        readBtn.textContent = "Not Read"
-        readBtn.style.cssText = "background-color: lightred;"
-      }
-
-    gridBooks.appendChild(bookCard)
-    bookCard.appendChild(title)
-    bookCard.appendChild(author)
-    bookCard.appendChild(pages)
-    bookCard.appendChild(readBtn)
-    bookCard.appendChild(removeBtn)
-
-    removeBtn.onclick = function(){
-      // bookIndex = this.parentNode.id
-      // myLibrary.splice(bookIndex,1)
-      // removeBtn.parentElement.remove();
-      console.log(myLibrary)
-      console.log(index)
-      }
+  if (book.haveRead === true){
+    readBtn.textContent = "Read"
+    readBtn.style.cssText = "background-color: lightgreen;"
+  } else {
+    readBtn.textContent = "Not Read"
+    readBtn.style.cssText = "background-color: lightred;"
   }
 
+  gridBooks.appendChild(bookCard)
+  bookCard.appendChild(title)
+  bookCard.appendChild(author)
+  bookCard.appendChild(pages)
+  bookCard.appendChild(readBtn)
+  bookCard.appendChild(removeBtn)
 
-
-  function renderBooks(){
-    myLibrary.map(function (book, index){
-      createBookItem(book, index)
-    })
+  removeBtn.onclick = function(){
+    myLibrary.splice(myLibrary.indexOf(book),1);
+    removeBtn.parentElement.remove();
+    setData()
+    console.log(myLibrary)
   }
 
+  readBtn.onclick = function(){
+    if (book.haveRead === true){
+      book.haveRead = false
+      readBtn.textContent = "Not Read"
+      readBtn.style.cssText = "background-color: lightred;"
+    } else {
+      book.haveRead = true
+      readBtn.textContent = "Read"
+      readBtn.style.cssText = "background-color: lightgreen;"
+    }
+    setData()
+  }
+}
+
+// applies the createBookItem function to all books in myLibrarys
+function renderBooks(){
+  myLibrary.map(function (book, index){
+    createBookItem(book, index)
+  })
+}
+
+// gets input from user and creates new book
 function  getInput(){
 const title = document.getElementById('title').value
 const author = document.getElementById('author').value
@@ -120,10 +131,24 @@ submit.onclick = function(){
   resetForm()
 }}
 
-// function addLocalStorage(){
-//   // myLibrary = JSON.parse(localStorage.getItem('library')) || []
-//   // renderBooks()
-// }
 
-renderBooks()
-console.log(myLibrary)
+// Local storage settings
+function setData(){
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
+}
+
+// Local storage when page is refreshed
+function restore() {
+  if(!localStorage.myLibrary) {
+    renderBooks();
+  }else {
+      let objects = localStorage.getItem('myLibrary') // gets information from local storage to use in below loop to create DOM/display
+      objects = JSON.parse(objects);
+      myLibrary = objects;
+      renderBooks();
+  }
+}
+
+restore();
+
+// localStorage.clear(); will reset localStorage to default
